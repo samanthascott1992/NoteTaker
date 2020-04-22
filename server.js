4501110ee6d287847d0f6793153c74c4be3dfdb7
Samantha Scott <Samanthascott1992@gmail.com>
2020-04-21 18:58:15 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const apiroutes = require("./routes/apiroutes");
+const htmlroutes = require("./routes/htmlroutes");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
+
+app.listen(PORT, ()=> console.log (PORT));                                      
+
+
+
+
